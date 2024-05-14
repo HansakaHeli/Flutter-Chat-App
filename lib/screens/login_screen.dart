c4d802e9e781b3bfc5986cc842aad5814d5cd3fa
HansakaHeli <programminghhr66@gmail.com>
2024-05-14 16:32:44 +0530

    Create User login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/components/rounded_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_chat_app/screens/chat_screen.dart';
+
 
 class LoginScreen extends StatefulWidget {
 
@@ -13,6 +17,30 @@ class _LoginScreenState extends State<LoginScreen> {
 
   late String email;
   late String password;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize Firebase when the screen is first created
+    print("Call ininstate");
+    initFirebase();
+  }
+
+  // Initialize Firebase
+  void initFirebase()  async{
+    print("calling initFirebase");
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: 'AIzaSyCq7qJPKDIzK8gq8ZUoKI6uyNHyN3P7zVs',
+            appId: '1:87105101281:android:a8d9d34a3aa22560b106a0',
+            messagingSenderId: '87105101281',
+            projectId: 'flash-chat-7c511'
+          // storageBucket: 'myapp-b9yt18.appspot.com',
+          // These are come form gogle-services.json file
+          // look -> https://www.youtube.com/watch?v=_M-GLwuWfoM
+        )
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -95,7 +123,26 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
               height: 24.0,
             ),
-            RoundedButton('Login', Colors.lightBlueAccent, () { }),
+            RoundedButton('Login', Colors.lightBlueAccent, () async{
+
+              try{
+                // Sign-in
+                UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                    email: email,
+                    password: password
+                );
+
+                if(userCredential != null){
+                  Navigator.pushNamed(context, ChatScreen.id);
+                }
+
+              }catch (e){
+                print(e);
+              }
+
+
+
+            }),
           ],
         ),
       ),
